15856e541ce839d2a4c6de711ffa500051753bae
Nicholas Parr <parrnicholasj@gmail.com>
2019-03-27 19:20:18 -0400

    javascript and html setup

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+
+
+var tries;
+var lettersGuess = [];
+var wins;
+var losses;
+var userGuess;
+var word;
+
+document.onkeyup = function(event) 
+{
+
+userGuess = event.key.toLowerCase();
+
+alert(userGuess);
+  
+}
