95905a5a87bf8563eef2a7703fb05ca55301c97b
parrn1 <mrnickp@comcast.net>
2019-03-28 14:34:36 -0400

    working on game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,24 +3,59 @@ var lettersGuessed = [];
 var wins;
 var losses;
 var userGuess;
-var word;
-var gameOn = true;//need to update this later
+var wordPool = ["doom", "argent", "energy", "marine", "demons"]
+var word = "test";
+var wordArray = [];
+var gameOn = false; //need to update this later
+
+var $game = document.getElementById("game");
+var $holder = document.getElementById("holder");
 
 document.onkeyup = function (event) {
   if (gameOn) {
 
-    userGuess = event.key.toLowerCase();
+    userGuess = event.key.toLowerCase(); //set to lower case so no capitals problem
+    console.log(userGuess);
 
-    lettersGuessed.push(userGuess)
+    lettersGuessed.push(userGuess); //add key to array
 
-    var targetDiv = document.getElementById("game");
-    targetDiv.textContent = "You guessed " + userGuess;
-    // In this example, we instead create a new div called "newDiv".
+    //var $game = document.getElementById("game");
+    $game.textContent = "You guessed " + userGuess; //tell what they just guessed
+
     var newDiv = document.createElement("div");
-    // We then give this newDiv the text "A pleasure to meet you!".
-    newDiv.textContent = "A pleasure to meet you!";
-    // Now we use the ".appendChild" method to combine the two divs together on the page.
-    targetDiv.appendChild(newDiv);
+
+    newDiv.textContent = "guessed letters: " + lettersGuessed;
+
+    $game.appendChild(newDiv); //add on to what letters were guess
+
+    //check if correct letter chosen
+
+
+  }
+
+  if (!gameOn) {
+
+    //first key press sets the word to be used
+
+    //word = wordPool[Math.floor(Math.random() * wordPool.length)];   //disabled for testing !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    //set the blank word segment
+
+    //var $game = document.getElementById("game");
+
+    for (var i = 0; i < word.length; i++) {
+
+      wordArray.push(word.charAt(i));
+
+      var blankSpan = document.createElement("span");
+      blankSpan.setAttribute("class", "letterSpace");
+      blankSpan.textContent = "-";
+      $holder.appendChild(blankSpan);
+
+      console.log(wordArray);
+    }
+
+    gameOn = true;
 
   }
 
